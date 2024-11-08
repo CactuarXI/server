456c3a7b3e13d19035e095e6bcfb5830d06ab403
CynicaGaming <CynicaGaming@gmail.com>
2024-11-07 22:39:36 -0500

    Added Buff Token (Self) and Buff Token (Party) on character creation.

--- modules/cactuar/lua/cactuar_create_char.lua
+++ modules/cactuar/lua/cactuar_create_char.lua
@@ -9,6 +9,8 @@ local m = Module:new('cactuar_create_char')
 m:addOverride('xi.player.charCreate', function(player)
     player:addLinkpearl('Cactuar', true)
     player:addItem(xi.item.SPROUT_BERET)
+    player:additem(xi.item.BUFF_TOKEN_SELF, 5)
+    player:additem(xi.item.BUFF_TOKEN_PARTY, 5)
     super(player)
 end)
 
