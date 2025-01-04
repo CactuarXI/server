8320720079c07f230954dc92bc38e0acccd879f4
Umeboshi <kuroi_neko91@live.com>
2025-01-03 23:36:04 -0800

    [lua][cactuar] FoV Page Bugfix Fixes incorrect mob index values.

--- scripts/zones/Rolanberry_Fields_[S]/mobs/Champion_Crawler.lua
+++ scripts/zones/Rolanberry_Fields_[S]/mobs/Champion_Crawler.lua
@@ -4,7 +4,7 @@
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
-    xi.cactuarRegimes.checkRegime(player, mob, 8, 1, xi.regime.type.FIELDS)
+    xi.cactuarRegimes.checkRegime(player, mob, 7, 1, xi.regime.type.FIELDS)
 end
 
 return entity
